46fc93f168acfcb4a1088c38e29c6c3b41247d1a
Johan Cortés <camilo4182@gmail.com>
2024-09-10 18:28:01 -0500

    feat(frontend): create grid of notes

--- safe-notes-frontend/src/components/NoteCard.jsx
+++ safe-notes-frontend/src/components/NoteCard.jsx
@@ -2,21 +2,25 @@ import React from 'react'
 import '../styles/NoteCard.css';
 import CategoryLabel from './CategoryLabel';
 
-export default function NoteCard() {
-  const note = {
-    title: 'Go for lunch',
-    status: 'Active',
-    date: 'April 01 2005',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum lacinia justo at mi malesuada, a tempus massa viverra. Ut egestas suscipit euismod. Praesent viverra varius sodales. Suspendisse dictum orci diam, sed consequat tortor hendrerit a. Sed sodales sapien sit amet lectus porta aliquet. Maecenas euismod mauris ut enim sagittis consequat.',
-    categories: [
-      <CategoryLabel key='1' category='Category 1' />,
-      <CategoryLabel key='2' category='Category 2' />,
-      <CategoryLabel key='3' category='Category 3' />,
-      <CategoryLabel key='4' category='Category 4' />,
-      <CategoryLabel key='5' category='Category 5' />,
-      <CategoryLabel key='6' category='Category 6' />
-    ]
-  };
+export default function NoteCard(props) {
+  // const note = {
+  //   title: 'Go for lunch',
+  //   status: 'Active',
+  //   date: 'April 01 2005',
+  //   text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum lacinia justo at mi malesuada, a tempus massa viverra. Ut egestas suscipit euismod. Praesent viverra varius sodales. Suspendisse dictum orci diam, sed consequat tortor hendrerit a. Sed sodales sapien sit amet lectus porta aliquet. Maecenas euismod mauris ut enim sagittis consequat.',
+  //   categories: [
+  //     <CategoryLabel key='1' category='Category 1' />,
+  //     <CategoryLabel key='2' category='Category 2' />,
+  //     <CategoryLabel key='3' category='Category 3' />,
+  //     <CategoryLabel key='4' category='Category 4' />,
+  //     <CategoryLabel key='5' category='Category 5' />,
+  //     <CategoryLabel key='6' category='Category 6' />
+  //   ]
+  // };
+  const categoryComponents = props.note.categories.map(
+    category => <CategoryLabel key={category} category={category} />
+  );
+  const note = props.note;
 
   return (
     <div className='card'>
@@ -34,7 +38,7 @@ export default function NoteCard() {
       </div>
       <div className='footer-card-div'>
         <div className='note-categories-div'>
-          {note.categories}
+          {categoryComponents}
         </div>
         <div className='delete-button-div'>
           <button className='delete-button'>X</button>
--- safe-notes-frontend/src/components/NotesGrid.jsx
+++ safe-notes-frontend/src/components/NotesGrid.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import NoteCard from './NoteCard'
+import React from 'react';
+import NoteCard from './NoteCard';
+import notes from '../data/notes-data.json';
+import '../styles/NotesGrid.css';
 
 export default function NotesGrid() {
+  const noteCardElements = notes.map(
+    note => <NoteCard key={note.id} note={note} />
+  );
+
   return (
     <div className='notes-grid'>
-      <NoteCard />
+      {noteCardElements}
     </div>
   )
 }
